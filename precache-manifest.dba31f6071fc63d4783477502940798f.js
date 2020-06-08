b97fd13382e46e2c1571b422c34dceae5e7fe1d6
Mayra Urrutia <mayratech@gmail.com>
2020-06-07 23:55:20 -0700

    Updates

---
+++ precache-manifest.dba31f6071fc63d4783477502940798f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dee0ef761465397e52763991245a641",
+    "url": "/react-site/index.html"
+  },
+  {
+    "revision": "c2fe7c6b72c50505b49d",
+    "url": "/react-site/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5aefc56371613c7b5122",
+    "url": "/react-site/static/js/2.b1605910.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-site/static/js/2.b1605910.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2fe7c6b72c50505b49d",
+    "url": "/react-site/static/js/main.12cef18f.chunk.js"
+  },
+  {
+    "revision": "1738ab7d3893cfa3df66",
+    "url": "/react-site/static/js/runtime-main.110db720.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-site/static/media/logo.5d5d9eef.svg"
+  }
+]);
